21d06fc8e0367244dd96abf45238f799eeeb4e9d
kreativevibesja-blip <kreativevibesja@gmail.com>
2026-03-17 00:08:24 -0500

    Delete dist/workers directory

--- dist/workers/analysisWorker.d.ts
+++
@@ -1,4 +0,0 @@
-import { Worker } from 'bullmq';
-import { type AnalysisJobData } from '../queues/analysisQueue';
-export declare const startAnalysisWorker: () => Worker<AnalysisJobData, any, string>;
-//# sourceMappingURL=analysisWorker.d.ts.map
